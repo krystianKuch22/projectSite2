f1ac9a5687586c15739b8d50e400a6d26b6b9201
Krystian <krystian_kucharski@wp.pl>
2023-03-21 01:51:13 +0100

    poroject done !!!

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,8 +1,35 @@
-const accordion = document.querySelector('.mywork__accordion')
-const accordionBtns = document.querySelectorAll('.mywork__btn')
+const accordion = document.querySelector('.mywork__accordion');
+const accordionBtns = document.querySelectorAll('.mywork__btn');
 
 function openAccordionItems() {
-    this.nextElementSibling.classList.toggle('active')
+	if (this.nextElementSibling.classList.contains('active')) {
+		this.nextElementSibling.classList.toggle('active');
+	} else {
+		closeAccordionItems();
+		this.nextElementSibling.classList.toggle('active');
+	}
 }
 
-accordionBtns.forEach(btn => btn.addEventListener('click', openAccordionItems))
+const closeAccordionItems = () => {
+	const allActiveItems = document.querySelectorAll('.mywork__info');
+	allActiveItems.forEach((item) => item.classList.remove('active'));
+};
+
+const clickOutsideAccordion = (e) => {
+	if (
+		e.target.classList.contains('mywork__btn') ||
+		e.target.classList.contains('mywork__info') ||
+		e.target.classList.contains('mywork__info-text') ||
+		e.target.classList.contains('mywork__img') ||
+		e.target.classList.contains('mywork__link')
+	)
+		return;
+
+	closeAccordionItems();
+};
+
+accordionBtns.forEach((btn) =>
+	btn.addEventListener('click', openAccordionItems)
+);
+
+window.addEventListener('click', clickOutsideAccordion);
--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const sliderBox = document.querySelector('.slider__box');
 const btnLeft = document.querySelector('.slider__btn-left');
 const btnRight = document.querySelector('.slider__btn-right');
 const allSections = document.querySelectorAll('.section');
+const gear = document.querySelectorAll('.deco-gear')
 
 const sliderBoxWidth = 25;
 const sliderSpeed = 3000;
@@ -32,5 +33,20 @@ const handleLeftArrow = () => {
     changeSection();
 }
 
+const rotateGear = () => {
+    let deg = 0;
+
+    setInterval(() => {
+        if(deg == 360){
+            deg = 0;
+        }
+        gear.forEach(item => {
+            item.style.transform = `translate(-50%) rotateZ(${deg+=0.5}deg)`
+        })
+        // gear.style.transform = `translate(-50%) rotateZ(${deg+=0.5}deg)`
+    }, 30)
+}
+
 btnLeft.addEventListener('click', handleLeftArrow)
 btnRight.addEventListener('click', handleRightArrow)
+window.addEventListener('DOMContentLoaded', rotateGear)
